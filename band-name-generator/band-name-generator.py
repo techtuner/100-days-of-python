d0e3daa71e0a7e2e2442712d6ba539befde47b0f
techtuner <techtuner284@gmail.com>
2025-07-25 09:55:39 +0530

    Day1 : Bandname Generator Project

---
+++ band-name-generator/band-name-generator.py
@@ -0,0 +1,10 @@
+print("=== Band Name Generator ===")
+print("-------------------------------------------------")
+
+
+
+print("Welcome to Band Generator")
+city = input("What's the name of the city you gre up in? ")
+pet = input("What's your pet's name? ")
+
+print(f"Your band name should be {city.capitalize()} {pet.capitalize()}")
